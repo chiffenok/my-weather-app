ae75d2bcd646d1740239c6e92cc7a878192a886c
Anna Presnyakova <apresnyakova88@gmail.com>
2022-12-18 22:58:57 +0100

    improve responsiveness for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Container, Toolbar, Typography } from '@mui/material'
 import TsunamiOutlinedIcon from '@mui/icons-material/TsunamiOutlined'
 
 const Header = () => (
   <AppBar position='static'>
-    <Toolbar sx={{ width: 500, m: '0 auto' }}>
-      <TsunamiOutlinedIcon fontSize='large' />
-      <Typography
-        variant='h4'
-        component='div'
-        sx={{ flexGrow: 1, textAlign: 'center', fontFamily: 'Pacifico' }}
-      >
-        My Weather App
-      </Typography>
-    </Toolbar>
+    <Container maxWidth='sm'>
+      <Toolbar>
+        <TsunamiOutlinedIcon fontSize='large' />
+        <Typography
+          variant='h4'
+          component='div'
+          sx={{ flexGrow: 1, textAlign: 'center', fontFamily: 'Pacifico' }}
+        >
+          My Weather App
+        </Typography>
+      </Toolbar>
+    </Container>
   </AppBar>
 )
 
